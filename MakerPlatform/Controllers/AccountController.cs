13a78802805c20d9d23dc6cc915f203c5098e79c
Padmate <959853840@qq.com>
2016-04-24 01:55:44 +0800

    refactor login page

--- MakerPlatform/Controllers/AccountController.cs
+++ MakerPlatform/Controllers/AccountController.cs
@@ -83,7 +83,7 @@ namespace MakerPlatform.Controllers
                 if (result.Succeeded)
                 {
                     await SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Default", "Home");
                 }
                 else
                 {
@@ -290,7 +290,7 @@ namespace MakerPlatform.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Default", "Home");
         }
 
         //
@@ -372,7 +372,7 @@ namespace MakerPlatform.Controllers
             }
             else
             {
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Default", "Home");
             }
         }
 
