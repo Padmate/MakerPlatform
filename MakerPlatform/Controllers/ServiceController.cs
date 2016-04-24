3e5f7732016f1d0f5d7ce37210248a5d37056c05
Padmate <959853840@qq.com>
2016-04-24 13:05:16 +0800

    Add service parts

---
+++ MakerPlatform/Controllers/ServiceController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MakerPlatform.Controllers
+{
+    public class ServiceController:Controller
+    {
+        //市场平台
+        public ActionResult MarketPlatform()
+        {
+            return View();
+        }
+
+        //品牌平台
+        public ActionResult BrandPlatform()
+        {
+
+            return View();
+        }
+
+        //销售平台
+        public ActionResult SellPlatform()
+        {
+
+            return View();
+        }
+
+        //制造平台
+        public ActionResult MakePlatform()
+        {
+
+            return View();
+        }
+
+        //工程平台
+        public ActionResult ProjectPlatform()
+        {
+
+            return View();
+        }
+
+        //产品平台
+        public ActionResult ProductPlatform()
+        {
+
+            return View();
+        }
+
+        //供应链管理
+        public ActionResult SupplyManagement()
+        {
+
+            return View();
+        }
+
+        //导师平台
+        public ActionResult TutorPlatform()
+        {
+
+            return View();
+        }
+
+        //资金平台
+        public ActionResult FundPlatform()
+        {
+
+            return View();
+        }
+    }
+}
